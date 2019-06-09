77ae2f1d50c03e3ca3504bb9cb27b07ff5bf442f
WLL-1017065322 <1017065322@qq.com>
2019-06-09 23:26:32 +0800

    111

---
+++ vuelogin001/src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h3>欢迎 {{name}}</h3>
+        <a href="#" @click="quit">注销登录</a>
+    </div>
+</template>
+
+<script>
+/*引入公共方法*/
+import { setCookie,getCookie,delCookie } from '../cookie.js'
+import { constants } from 'crypto';
+    export default{
+        data(){
+            return{
+                name: ''
+            }
+        },
+        mounted(){
+            /*页面挂载获取保存的cookie值，渲染到页面上*/
+            let uname = getCookie('username')
+            this.name = uname
+            /*如果cookie不存在，则跳转到登录页*/
+            if(uname == ""){
+                this.$router.push('/')
+            }
+        },
+        methods:{
+            quit(){
+                /*删除cookie*/
+                console.log(this.name)
+                delCookie('username')
+                
+
+                // 自己添的
+                // this.$router.push('/')
+            }
+        }
+    }
+</script>
